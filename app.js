7a46b4debc50f6415a2fe2ba6c5dab3689711686
Amulya Boggavarapu <82016332+amulyaboggavarapu7@users.noreply.github.com>
2023-10-29 21:49:01 -0500

    new commit

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var EarringsRouter=require('./routes/Earrings')
+var EarringsRouter=require('./routes/Earrings');
+var boardRouter=require('./routes/board');
+
 
 var app = express();
 
@@ -23,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Earrings', EarringsRouter);
-
+app.use('/board', boardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
