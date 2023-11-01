3a1bee0752596870085d0aab2bb0a61d01a8df0a
Amulya Boggavarapu <82016332+amulyaboggavarapu7@users.noreply.github.com>
2023-11-01 14:22:26 -0500

    new commit

--- routes/board.js
+++ routes/board.js
@@ -1,25 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+ 
 /* GET home page. */
-router.get('/gridbuild', function (req, res) {
-    let query = req.query;
-    console.log(`rows ${query.rows}`);
-    console.log(`cols ${query.cols}`);
-    res.render('board', { title: 'Board Display', query: query });
-    function computeCellValue(row, col, rows, cols) {
-        if (row === col) {
-          return 1;
-        } else if (row === col + 1) {
-          return 2;
-        } else if (row === col - 1) {
-          return row;
-        }
-        return 0;
-      }
-      
-      
-      
+router.get('/', function(req, res, next) {
+  let query = req.query;
+  console.log(`rows:${query.row}`)
+  console.log(`cols:${query.cols}`)
+  res.render('board', { title: 'Board Display',query:query })
 });
-
+ 
 module.exports = router;
