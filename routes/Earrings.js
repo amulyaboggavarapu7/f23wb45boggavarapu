d5ecc5321602e1efdbe5ca6144bab96074f80c74
Amulya Boggavarapu <82016332+amulyaboggavarapu7@users.noreply.github.com>
2023-10-31 21:34:33 -0500

    new commit

--- routes/Earrings.js
+++ routes/Earrings.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('Earrings', { title: 'Search Results Earrings' });
+  
 });
 
 module.exports = router;
